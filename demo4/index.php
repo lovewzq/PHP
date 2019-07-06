5d5c0abc8c0bcbcba168a4ed77091ddcda07baed
yuan <2403411375@qq.com>
2019-07-06 22:42:43 +0800

    简单的PHP原生博客

---
+++ demo4/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="static/css/mian.css">
+    <link rel="stylesheet" type="text/css" href="static/plugins/bootstrap/css/bootstrap.min.css">
+
+    <title>博客</title>
+</head>
+<body>
+    <div class="head"></div>
+</body>
+</html>
